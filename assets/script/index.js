5df7b12c014e99b266d1ff3f725bd997e6f29964
av6920337 <av6920337@gmail.com>
2024-06-13 05:53:25 +0200

    The first commit

---
+++ assets/script/index.js
@@ -0,0 +1,11 @@
+const MessageForUser = () =>'Я учу JavaScript!';
+console.log(MessageForUser());
+
+// const btnPrevious = document.getElementById('btnPrevious');
+// const btnNext =document.getElementById('btnNext');
+// const img =document.querySelector('.img');
+
+// const nextImg = () =>{
+//     img.src ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSga1jAisgQKSypHnEDTsvctSm9pSBEzEZa-g&s';
+// };
+// btnNext.addEventListener('click', nextImg);
